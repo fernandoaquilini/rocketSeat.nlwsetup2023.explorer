5749569c256da80d612d659f181b9d76bae67b3a
fernandoaquilini <fernando.aquilini@gmail.com>
2023-01-21 12:25:46 -0300

    Aula 04
    Aula 04

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # rocketSeat.nlwsetup2023.explorer
- 
+
+<h1 align="center"> Habits! Trilha Explorer</h1>
+
+<p align="center">
+Treimaneto promovido pela <a href="https://rocketseat.com.br/">@Rocketseat.</a> para aprimoramento de conteúdos HTML, CSS e  JavaScript, Git e Figma. <br/>
+</p>
+<br>
+
+<p align="center">
+  <img alt="App Habits" src="./assets/cover.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+- Figma
+
+</br>
+
+Desenvolvivo por [Fernando Aquilini!](https://github.com/fernandoaquilini)
--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-bt").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Data já Existe! ⛔")
+    return
+  }
+
+  nlwSetup.addDay(today)
+  alert("Adicionado com Sucesso! ✅")
+}
+
+function save(){
+  localStorage.setItem("NLWSetup@habbits", JSON.stringify(nlwSetup.data))
+}
+
+/*
 const data = {
   run: ["01-01", "01-02", "01-06"],
   water: ["01-04", "01-05"],
@@ -8,6 +30,7 @@ const data = {
   journal: ["01-01", "01-02", "01-03", "01-05"],
   pills: ["01-04", "01-05"],
 }
-
+*/
+const data = JSON.parse(localStorage.getItem("NLWSetup@habbits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
